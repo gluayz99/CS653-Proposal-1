47be20ad3404565578cfc6bca67ff0e139f0a955
WATCHP_ <watchara.phorung@cmu.ac.th>
2023-05-26 23:27:15 +0700

    update plots

--- analyst-data.py
+++ analyst-data.py
@@ -4,6 +4,9 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
+    list_week = []
+    list_year = []
+    list_month = []
     directory = 's3'
     for root, dirs, files in os.walk(directory):
         for file in files:
@@ -21,7 +24,13 @@ if __name__ == "__main__":
                 print(df_month)
                 df_month.plot(y=['WQI'], figsize=(9,6))
                 plt.title(file)
-                plt.savefig(f"plots/{file.split('.')[0]}_by_month.png")
+                plt.savefig(f"plots/month/0-{file.split('.')[0]}_by_month.png")
+                plt.show()
+
+                df_month = df_month.sort_values()
+                df_month.plot(y=['WQI'], figsize=(9, 6))
+                plt.title(file)
+                plt.savefig(f"plots/month/1-{file.split('.')[0]}_by_month_sorted.png")
                 plt.show()
 
                 df_year = df.groupby(df['date'].dt.strftime('%Y'))['WQI'].mean()
@@ -29,7 +38,13 @@ if __name__ == "__main__":
                 print(df_year)
                 df_year.plot(y=['WQI'], figsize=(9, 6))
                 plt.title(file)
-                plt.savefig(f"plots/{file.split('.')[0]}_by_year.png")
+                plt.savefig(f"plots/year/0-{file.split('.')[0]}_by_year.png")
+                plt.show()
+
+                df_year = df_year.sort_values()
+                df_year.plot(y=['WQI'], figsize=(9, 6))
+                plt.title(file)
+                plt.savefig(f"plots/year/1-{file.split('.')[0]}_by_year_sorted.png")
                 plt.show()
 
                 df_week = df.groupby(df['date'].dt.strftime('%W'))['WQI'].mean()
@@ -37,5 +52,11 @@ if __name__ == "__main__":
                 print(df_week)
                 df_week.plot(y=['WQI'], figsize=(9, 6))
                 plt.title(file)
-                plt.savefig(f"plots/{file.split('.')[0]}_by_week.png")
+                plt.savefig(f"plots/week/0-{file.split('.')[0]}_by_week.png")
+                plt.show()
+
+                df_week = df_week.sort_values()
+                df_week.plot(y=['WQI'], figsize=(9, 6))
+                plt.title(file)
+                plt.savefig(f"plots/week/1-{file.split('.')[0]}_by_week_sorted.png")
                 plt.show()
